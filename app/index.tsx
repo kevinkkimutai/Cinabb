ca81aea721ffb608f2eaa3a3052cb1fbee804ae9
Kelvin Kimutai <kiruikevinkim@gmail.com>
2024-03-16 14:02:44 +0300

    addded

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <WebView
+        source={{ uri: 'https://cinab.co.ke' }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
